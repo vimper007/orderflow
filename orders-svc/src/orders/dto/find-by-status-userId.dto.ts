211bae864247d018d08a25af750c165407d9a952
Vimukthi Perera <vimper007@gmail.com>
2025-11-29 23:58:03 +0530

    fix: correct formatting in FindByStatusOrUserIdDto for better readability

--- orders-svc/src/orders/dto/find-by-status-userId.dto.ts
+++ orders-svc/src/orders/dto/find-by-status-userId.dto.ts
@@ -2,9 +2,9 @@ import { IsEmpty, IsEnum, IsOptional, IsString } from "class-validator";
 import { OrderStatus } from "../types/order.types";
 
 export class FindByStatusOrUserIdDto {
-    
+
     @IsOptional()
-    @IsEnum(OrderStatus)
+    @IsEnum(OrderStatus) 
     status:OrderStatus;
 
     @IsOptional()
